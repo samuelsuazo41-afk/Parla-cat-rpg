0d063434bcf90977221ee7af9e900b315166cd7d
samuelsuazo41-afk <samuelsuazo41@gmail.com>
2026-05-17 22:03:25 +0200

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'parla-cat-rpg-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './main.js',
+  './manifest.json',
+  './data/items.json',
+  './capitols/capitol1_bcn_born.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
